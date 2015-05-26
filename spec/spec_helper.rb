afd9a55b5f22c1de0f37a329c4ab867050f76c98
Robert O'Connor <rob@oconnor.ninja>
2015-05-25 20:47:58 -0400

    fix require in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'utils'
+require 'board'
 
 RSpec.configure do |config|
   config.include Utils
