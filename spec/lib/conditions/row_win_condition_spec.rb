70a9d7ea9d477773d70540e2a9e37025525274e8
Robert O'Connor <rob@oconnor.ninja>
2015-06-02 05:46:14 -0400

    win condition tests are written

---
+++ spec/lib/conditions/row_win_condition_spec.rb
@@ -0,0 +1,13 @@
+describe RowWinCondition do
+  describe "#win?" do
+    let!(:win_condition) { build(:row_win_condition, :winner)}
+    let!(:draw) { build(:row_win_condition, :draw)}
+
+    it "returns true if a winner is in a column" do
+      expect(win_condition.win?).to be true
+    end
+    it "returns false if there is not a winner in any column" do
+      expect(draw.win?).to be false
+    end
+  end
+end
